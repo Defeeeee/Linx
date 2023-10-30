4c9c05125a21bb4185c03ce26e45210ae08956e3
Federico Diaz Nemeth <81602924+Defeeeee@users.noreply.github.com>
2023-10-30 08:24:01 -0300

    update //

--- pidv3/pidv3.ino
+++ pidv3/pidv3.ino
@@ -11,25 +11,20 @@
 
 PID PID(5, 1.2, 0.5);
 
-int analogPins[] = {A5, A6, A7, A8, A9, A10, A11, A12, A13, A14};
-float valorsensor[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-float valorsensorant[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int analogPins[] = {A5, A6, A7, A8, A9, A10, A11, A12, A13, A14}; // 10 sensores
+float valorsensor[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; 
 
-int vel = 100;
-int smooth = 1;
-SensorCalibration cal(10, 80, 55);
+int vel = 90; // 0-255
+SensorCalibration cal(10, 80, 55); // 10 sensores, velocidad "80" de impulso, 55 de constante
 
-float step = 0;
+float step = 0; // tiempo de cada iteracion
 
 void setup()
 {
     // put your setup code here, to run once:
     Serial.begin(115200);
-    cal.calibrate(2000, analogPins);
+    cal.calibrate(2000, analogPins); // 2000 iteraciones de calibracion
     delay(3000);
-    for (int i = 0; i < 10; i++){
-        valorsensor[i] = cal.getCalibratedValue(i, analogPins);
-    }
 }
 
 void loop()
@@ -37,54 +32,54 @@ void loop()
     // put your main code here, to run repeatedly:
     long pt = micros();
     int error = 0;
-    
+
     for (int i = 0; i < 10; i++)
     {
-        
-        valorsensor[i] = (cal.getCalibratedValue(i, analogPins))*0.9 + valorsensorant[i]*0.1;
-        valorsensorant[i] = valorsensor[i];
 
-        if (valorsensor[i] < 0.9)
+        valorsensor[i] = cal.getCalibratedValue(i, analogPins); // obtiene el valor calibrado de cada sensor
+
+        if (valorsensor[i] < 0.9) // si el valor es menor a 0.9, se considera que esta en la linea
         {
             valorsensor[i] = 1;
         }
-        
     }
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++) // calcula el error del lado izquierdo
     {
-        if (valorsensor[i] == 1)
+        if (valorsensor[i] == 1) 
         {
             error--;
         }
     }
-    for (int i = 5; i < 10; i++)
+    for (int i = 5; i < 10; i++) // calcula el error del lado derecho
     {
-        if (valorsensor[i] == 1)
+        if (valorsensor[i] == 1) 
         {
             error++;
         }
     }
 
     Serial.println(error);
-    float output = PID.pid(error, step);
-        digitalWrite(PINMOTOR1A, LOW);
-        digitalWrite(PINMOTOR1B, HIGH);
-        digitalWrite(PINMOTOR2A, LOW);
-        digitalWrite(PINMOTOR2B, HIGH);
-    if (output > 0)
-    {
-        
+    float output = PID.pid(error, step); // calcula la salida del PID
 
-        analogWrite(PINMOTORPWM1, vel - output * vel);
+    digitalWrite(PINMOTOR1A, LOW); 
+    digitalWrite(PINMOTOR1B, HIGH);
+    digitalWrite(PINMOTOR2A, LOW);
+    digitalWrite(PINMOTOR2B, HIGH);
+    if (output > 0) // si la salida es positiva, el motor izquierdo gira mas rapido
+    {
+
+        analogWrite(PINMOTORPWM1, vel - output * vel); 
         analogWrite(PINMOTORPWM2, vel);
     }
-    else
+    else if (output < 0) // si la salida es negativa, el motor derecho gira mas rapido
     {
         analogWrite(PINMOTORPWM1, vel);
-        analogWrite(PINMOTORPWM2, vel - abs(output) * vel);
+        analogWrite(PINMOTORPWM2, vel - abs(output) * vel); 
+    } else { // si la salida es 0, ambos motores giran a la misma velocidad
+        analogWrite(PINMOTORPWM1, vel);
+        analogWrite(PINMOTORPWM2, vel);
     }
 
-
-    unsigned long step_micros = micros() - pt;
-    step = (float)step_micros / 1000000;
+    unsigned long step_micros = micros() - pt; // calcula el tiempo de cada iteracion
+    step = (float)step_micros / 1000000; // convierte el tiempo a segundos
 }
