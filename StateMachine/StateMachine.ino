b1763eb81b033e23a3b9b6905cf17754d6dcf4aa
Federico Diaz Nemeth <81602924+Defeeeee@users.noreply.github.com>
2023-11-20 16:25:48 -0300

    some comments

--- StateMachine/StateMachine.ino
+++ StateMachine/StateMachine.ino
@@ -54,19 +54,23 @@
  * @note This code is for illustrative purposes and may require additional modifications for specific robot configurations.
  */
 
-enum RobotState {
+// States
+enum RobotState { 
     IDLE,
     CALIBRATION,
     LINE_FOLLOWING,
     STOP
 };
 
+// Current state
 RobotState currentState = CALIBRATION;
 
+// Pin definitions
 #define PINMOTOR1A 8
 #define PINMOTOR1B 9
 #define PINMOTOR2A 10
 #define PINMOTOR2B 11
+
 #define trig2 2
 #define echo2 3
 #define trig1 4
@@ -75,38 +79,43 @@ RobotState currentState = CALIBRATION;
 #define PINMOTORPWM1 13
 #define PINMOTORPWM2 12
 
+int analogPins[] = {A5, A6, A7, A8, A9, A10, A11, A12, A13, A14};
+
+// PID controller, ultrasonic sensor, and sensor calibration objects
 PID PID(3, 0.5, 0.3);
 Ultrasonico Ult(trig1, echo1, trig2, echo2);
-
-int analogPins[] = {A5, A6, A7, A8, A9, A10, A11, A12, A13, A14};
-float valorsensor[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-int vel = 200;
 SensorCalibration cal(10, 150, 90);
 
-float step = 0;
-int stop_index = 0;
+// Sensor values storing arrays
+float valorsensor[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-int stops = 5;
+// Motor velocity
+int vel = 200;
+
+float step = 0; // Time step
+int stop_index = 0; // Current stop index
+
+int stops = 5; // Total number of stops
 
 int stop_type[] = { 1, 2, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 2, 1, 2, 0, 0, 1, 1, 0, 0, 1, 2, 3, 0, 0, 0}; // 0 = aula, 1 = izquierda, 2 = derecha, 3 = ?
-int wall_distance[] = { 30, 150, 150, 150, 150, 150, 250, 30, 150, 150, 150, 150, 150, 30, 150, 150, 150, 50, 30, 150, 150, 30, 150, 200, 150, 150, 150};
+int wall_distance[] = { 30, 150, 150, 150, 150, 150, 250, 30, 150, 150, 150, 150, 150, 30, 150, 150, 150, 50, 30, 150, 150, 30, 150, 200, 150, 150, 150}; // Distancia de la pared
 
 void setup()
 {
     Serial.begin(115200);
-    cal.calibrate(2000, analogPins);
-    delay(3000);
 }
 
 void loop()
 {
+    // Check for serial input
     if (Serial.available()) {
         char input = Serial.read();
         if (input == "stop") {
             currentState = STOP;
         }
     }
+
+    // State machine
     switch (currentState) {
         case IDLE:
             idleState();
@@ -123,6 +132,7 @@ void loop()
     }
 }
 
+// State functions
 void idleState() {
     if (Serial.available()) {
         char input = Serial.read();
@@ -135,6 +145,7 @@ void idleState() {
 void calibrationState() {
     Serial.println("Calibrating...");
     cal.calibrate(2000, analogPins);
+    delay(3000);
     currentState = IDLE;
 }
 
@@ -239,6 +250,7 @@ int CalcErr()
     return error;
 }
 
+// Controls the motors based on the PID output.
 void Avanzar(float output)
 {
     digitalWrite(PINMOTOR1A, LOW);
