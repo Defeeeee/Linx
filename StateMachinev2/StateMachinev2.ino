bee9a4f9412ba4c057f62f15e7c5d4db6281ef9b
Federico Diaz Nemeth <81602924+Defeeeee@users.noreply.github.com>
2024-01-06 14:09:48 -0300

    Update Arduino configuration and libraries

---
+++ StateMachinev2/StateMachinev2.ino
@@ -0,0 +1,222 @@
+#include <SensorCalibration.h>
+#include <PIDv1.h>
+#include <Ultrasonico.h>
+
+// /**
+//  * @file StateMachine.ino
+//  * @brief This file contains the implementation of a state machine for a robot.
+//  * The robot has different states such as IDLE, CALIBRATION, LINE_FOLLOWING, and STOP.
+//  * It uses sensor calibration, PID control, and ultrasonic sensors for line following and obstacle detection.
+//  * The robot can be controlled through serial communication.
+//  *
+//  * @author Manuel Rao, Federico Diaz Nemeth
+//  *
+//  * @details
+//  * The state machine transitions between different states based on the current state and input received through serial communication.
+//  * The robot starts in the CALIBRATION state where it calibrates the sensors.
+//  * After calibration, it transitions to the IDLE state where it waits for user input to start line following.
+//  * In the LINE_FOLLOWING state, the robot follows a line using PID control and detects obstacles using ultrasonic sensors.
+//  * It stops at predefined stops based on the stop type and wall distance.
+//  * Once all stops are completed, it transitions to the STOP state where it stops the motors and returns to the IDLE state.
+//  *
+//  * The robot uses the following pins for motor control and sensor connections:
+//  * - Motor 1A: Pin 8
+//  * - Motor 1B: Pin 9
+//  * - Motor 2A: Pin 10
+//  * - Motor 2B: Pin 11
+//  * - Ultrasonic Sensor 1 Trigger: Pin 4
+//  * - Ultrasonic Sensor 1 Echo: Pin 5
+//  * - Ultrasonic Sensor 2 Trigger: Pin 2
+//  * - Ultrasonic Sensor 2 Echo: Pin 3
+//  * - Motor 1 PWM: Pin 13
+//  * - Motor 2 PWM: Pin 12
+//  *
+//  * The robot uses a PID controller for line following, with the following PID constants:
+//  * - Proportional Gain: 3
+//  * - Integral Gain: 0.5
+//  * - Derivative Gain: 0.3
+//  *
+//  * The robot also uses sensor calibration to calibrate the sensor values.
+//  * It uses 10 analog pins for sensor readings and stores the calibrated values in an array.
+//  *
+//  * The robot has a total of 5 stops, each with a stop type and wall distance.
+//  * The stop types are defined as follows:
+//  * - 0: Aula
+//  * - 1: Left
+//  * - 2: Right
+//  * - 3: Unknown
+//  *
+//  * The wall distances are defined for each stop and determine when the robot should stop or change direction.
+//  *
+//  * The robot's main loop checks for serial input and transitions between states based on the input and current state.
+//  * The robot also has helper functions for each state and motor control.
+//  *
+//  * @note This code is for illustrative purposes and may require additional modifications for specific robot configurations.
+//  */
+
+// States
+enum RobotState {
+    IDLE,
+    CALIBRATION,
+    LINE_FOLLOWING,
+    STOP
+};
+
+// Current state
+RobotState currentState = CALIBRATION;
+
+// Pin definitions
+#define PINMOTOR1A 8
+#define PINMOTOR1B 9
+#define PINMOTOR2A 10
+#define PINMOTOR2B 11
+
+#define PINMOTORPWM1 13
+#define PINMOTORPWM2 12
+
+int analogPins[] = {A5, A6, A7, A8, A9, A10, A11, A12, A13, A14};
+
+// // PID controller, ultrasonic sensor, and sensor calibration objects
+PID PID(3, 0.5, 0.3);
+SensorCalibration cal(10, 150, 90);
+
+// Sensor values storing arrays
+float valorsensor[] = {0, 0, 1, 1, 1, 1, 1, 1, 0, 0};
+
+// Motor velocity
+int vel = 120;
+
+float step = 0; // Time step
+
+void setup()
+{
+    Serial.begin(115200);
+  Serial.begin(115200);
+  myController.begin(&input, &output, &setpoint, p, i, d);
+  myController.setOutputLimits(-25500, 255000);
+  myController.setBias(255.0 / 2.0);
+  myController.setWindUpLimits(-100, 100); 
+  myController.start();
+  
+  // ADD MORE CONFIGURATION COMMANDS HERE <--------------------
+}
+
+
+void loop()
+{
+
+    // State machine
+    switch (currentState) {
+        case IDLE:
+            idleState();
+            break;
+        case CALIBRATION:
+            calibrationState();
+            break;
+        case LINE_FOLLOWING:
+            lineFollowingState();
+            break;
+        case STOP:
+            stopState();
+            break;
+    }
+}
+
+// State functions
+void idleState() {
+    if (Serial.available()) {
+        currentState = LINE_FOLLOWING;
+    }
+}
+
+void calibrationState() {
+    Serial.println("Calibrating...");
+    cal.calibrate(2000, analogPins);
+    delay(3000);
+    currentState = IDLE;
+}
+
+void lineFollowingState(int err) {
+    // Serial.println("Line following...");
+        long pt = micros();
+        // Avanzar(PID.pid(calcErr(), step));
+        Serial.println(PID.pid(err, step));
+        delay(10);
+        step = (micros() - pt) / 1000000.0;
+}
+
+void stopState()
+{
+    digitalWrite(PINMOTOR1A, LOW);
+    digitalWrite(PINMOTOR1B, LOW);
+    digitalWrite(PINMOTOR2A, LOW);
+    digitalWrite(PINMOTOR2B, LOW);
+    analogWrite(PINMOTORPWM1, 0);
+    analogWrite(PINMOTORPWM2, 0);
+    currentState = IDLE;
+}
+
+void RotarEnLugar(int dir)
+{
+    if (dir > 0)
+    {
+        digitalWrite(PINMOTOR1A, LOW);
+        digitalWrite(PINMOTOR1B, HIGH);
+        digitalWrite(PINMOTOR2A, HIGH);
+        digitalWrite(PINMOTOR2B, LOW);
+        analogWrite(PINMOTORPWM1, vel * dir);
+        analogWrite(PINMOTORPWM2, vel * dir);
+    }
+    else if (dir < 0)
+    {
+        digitalWrite(PINMOTOR1A, HIGH);
+        digitalWrite(PINMOTOR1B, LOW);
+        digitalWrite(PINMOTOR2A, LOW);
+        digitalWrite(PINMOTOR2B, HIGH);
+        analogWrite(PINMOTORPWM1, vel * abs(dir));
+        analogWrite(PINMOTORPWM2, vel * abs(dir));
+    }
+}
+
+// Calculates the error based on the calibrated sensor values.
+// Returns the calculated error.
+int calcErr()
+{
+    int error = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        // valorsensor[i] = cal.getCalibratedValue(i, analogPins);
+        // if (valorsensor[i] < 0.95)
+        // {
+        //     valorsensor[i] = 1;
+        // }
+        if (valorsensor[i] == 1) {
+            error += (i < 5) ? ((i >= 3 && i <= 6) ? 2 : 1) : ((i >= 3 && i <= 6) ? -2 : -1);
+        }
+    }
+    return error;
+}
+
+// Controls the motors based on the PID output.
+void Avanzar(float output)
+{
+    digitalWrite(PINMOTOR1A, LOW);
+    digitalWrite(PINMOTOR1B, HIGH);
+    digitalWrite(PINMOTOR2A, LOW);
+    digitalWrite(PINMOTOR2B, HIGH);
+    if (output > 0) // Turn Right
+    {
+        analogWrite(PINMOTORPWM1, vel - output * vel);
+        analogWrite(PINMOTORPWM2, vel - output * vel);
+    }
+    else if (output < 0) // Turn Left
+    {
+        analogWrite(PINMOTORPWM1, vel + abs(output) * vel);
+        analogWrite(PINMOTORPWM2, vel - abs(output) * vel);
+    }
+    else
+    {
+        analogWrite(PINMOTORPWM1, vel);
+        analogWrite(PINMOTORPWM2, vel);
+    }
+}
